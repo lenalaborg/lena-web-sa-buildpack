31c322d0afeea022f3baf28877367a75c686b34e
원송연 책임 아키텍처솔루션팀 (songyeon.won@lgcns.com <76882@CORP.LGCNS.COM>
2020-02-05 18:33:09 +0900

    Update lenaw.rb

--- lib/java_buildpack/container/lenaw.rb
+++ lib/java_buildpack/container/lenaw.rb
@@ -65,7 +65,7 @@ module JavaBuildpack
           @droplet.environment_variables.as_env_vars,
           @droplet.java_home.as_env_var,
           'exec',
-          "$PWD/#{(@droplet.sandbox + 'start.sh').relative_path_from(@droplet.root)}",
+          "$PWD/#{(@droplet.sandbox + 'servers/webServer/start.sh').relative_path_from(@droplet.root)}",
           'run'
         ].flatten.compact.join(' ')
 
